41f792c40fc4e16551c3c7542e5d32be49e12845
Giuliana Villarroel <giu.villarroel@gmail.com>
2026-01-27 22:12:11 -0300

    Add: loadAbout en la lógica de navegación entre pestañas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./style.css";
 import loadHome from "./pages/home.js";
 import loadMenu from "./pages/menu.js";
+import loadAbout from "./pages/about.js";
 
 const headerBtns = document.querySelectorAll(".btn");
 const menuBtn = document.querySelector("#menu-btn");
 const homeBtn = document.querySelector("#home-btn");
+const aboutBtn = document.querySelector("#about-btn");
 
 headerBtns.forEach((btn) => {
 	btn.addEventListener("click", () => {
@@ -17,5 +19,6 @@ loadHome();
 
 homeBtn.addEventListener("click", () => loadHome());
 menuBtn.addEventListener("click", () => loadMenu());
+aboutBtn.addEventListener("click", () => loadAbout());
 
 console.log("Webpack funcionando!");
