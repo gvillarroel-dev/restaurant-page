e305db8322dde5ad419730bfa1baffdfde4f0436
Giuliana Villarroel <giu.villarroel@gmail.com>
2026-01-28 22:21:56 -0300

    Add: eventListener a contactBtn para añadir el módulo contact.js a la lógica de navegación entre pestañas

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import "./style.css";
 import loadHome from "./pages/home.js";
 import loadMenu from "./pages/menu.js";
 import loadAbout from "./pages/about.js";
+import loadContact from "./pages/contact.js";
 
 const headerBtns = document.querySelectorAll(".btn");
 const menuBtn = document.querySelector("#menu-btn");
 const homeBtn = document.querySelector("#home-btn");
 const aboutBtn = document.querySelector("#about-btn");
+const contactBtn = document.querySelector("#contact-btn");
 
 headerBtns.forEach((btn) => {
 	btn.addEventListener("click", () => {
@@ -20,5 +22,6 @@ loadHome();
 homeBtn.addEventListener("click", () => loadHome());
 menuBtn.addEventListener("click", () => loadMenu());
 aboutBtn.addEventListener("click", () => loadAbout());
+contactBtn.addEventListener("click", () => loadContact());
 
 console.log("Webpack funcionando!");
